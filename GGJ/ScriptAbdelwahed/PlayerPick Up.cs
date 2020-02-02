d8ff04f2e7cc9547bc74de875a7ac9edbdf56ddd
Neoabdo <55669314+Neoabdo@users.noreply.github.com>
2020-02-02 13:07:21 +0100

    PlayerPickUp script V1
    Ajout Script

---
+++ GGJ/ScriptAbdelwahed/PlayerPick Up.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickUp : MonoBehaviour
+{/*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     */
+    void OnTriggerEnter(Collider other)
+    {
+        string ObjectName = other.gameObject.tag;
+        switch (ObjectName)
+        {
+            case "leftHand":
+                other.gameObject.SetActive(false);
+                break;
+
+            case "rightHand":
+                other.gameObject.SetActive(false);
+                break;
+
+            case "body":
+                other.gameObject.SetActive(false);
+                break;
+
+            case "heart":
+                other.gameObject.SetActive(false);
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+            
+    
